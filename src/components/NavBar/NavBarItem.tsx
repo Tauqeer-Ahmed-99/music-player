1370e9a6b09d40f28bcf96edde91e0a4291ff8c9
Tauqeer Khan <tauqueerrkhan@gmail.com>
2022-12-20 17:59:45 +0530

    added : invert to icons

--- src/components/NavBar/NavBarItem.tsx
+++ src/components/NavBar/NavBarItem.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
+import ThemeContext, { Theme } from "../../context/ThemeContext/ThemeContext";
 
 const NavBarItem = ({ icon, label }: { icon: any; label: string }) => {
+  const themeContext = useContext(ThemeContext);
+
   return (
     <li className="flex justify-center w-full h-10 pl-4 rounded-md btn-ghost btn hover:cursor-pointer hover:bg-primary">
       <img
@@ -8,7 +11,9 @@ const NavBarItem = ({ icon, label }: { icon: any; label: string }) => {
         alt="Icon"
         height={55}
         width={55}
-        className="hover:bg-transparent"
+        className={`hover:bg-transparent${
+          themeContext.theme === Theme.Business ? " invert" : ""
+        } delay-700`}
       />
       <span className="flex items-center w-full pl-0 hover:bg-transparent">
         {label}
